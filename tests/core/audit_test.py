61cf2d5c1b9f2f530dc26a58bf83d2b0ed69dfdc
Aaron Loo <aaronloo@yelp.com>
2018-06-26 16:09:01 -0700

    fixing tests

--- tests/core/audit_test.py
+++ tests/core/audit_test.py
@@ -36,6 +36,7 @@ class TestAuditBaseline(object):
         modified_baseline = self.baseline.copy()
         modified_baseline['results']['filenameA'][0]['is_secret'] = True
         modified_baseline['results']['filenameA'][1]['is_secret'] = False
+        modified_baseline['results']['filenameB'][0]['is_secret'] = False
 
         with self.mock_env(baseline=modified_baseline):
             audit.audit_baseline('will_be_mocked')
@@ -46,8 +47,9 @@ class TestAuditBaseline(object):
         modified_baseline = self.baseline.copy()
         modified_baseline['results']['filenameA'][0]['is_secret'] = True
         modified_baseline['results']['filenameA'][1]['is_secret'] = False
+        modified_baseline['results']['filenameB'][0]['is_secret'] = False
 
-        self.run_logic(['y', 'n'], modified_baseline)
+        self.run_logic(['y', 'n', 'n'], modified_baseline)
 
         assert mock_printer.message == (
             'Saving progress...\n'
@@ -67,8 +69,9 @@ class TestAuditBaseline(object):
     def test_skip_decision(self, mock_printer):
         modified_baseline = self.baseline.copy()
         modified_baseline['results']['filenameA'][1]['is_secret'] = True
+        modified_baseline['results']['filenameB'][0]['is_secret'] = True
 
-        self.run_logic(['s', 'y'], modified_baseline)
+        self.run_logic(['s', 'y', 'y'], modified_baseline)
 
         assert mock_printer.message == (
             'Saving progress...\n'
@@ -439,17 +442,16 @@ def mock_user_input(inputs):
     :type inputs: list
     :param inputs: list of user choices
     """
-    current_case = {'index': 0}     # needed, because py2 doesn't have nonlocal
-
     class InputShim(object):
         def __init__(self):
             self.message = ''
+            self.index = 0
 
         def get_user_input(self, *args, **kwargs):
             self.message += args[0]
 
-            output = inputs[current_case['index']]
-            current_case['index'] += 1
+            output = inputs[self.index]
+            self.index += 1
 
             return output
 
