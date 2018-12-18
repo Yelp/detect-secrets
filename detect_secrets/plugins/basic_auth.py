4886e04b849f118018c2d53efbe5a61ae3d0f8a7
Joshua Li <joshuali@yelp.com>
2018-12-17 16:37:22 -0800

    refactor BasicAuthDetector to use RegexBasedDetector

--- detect_secrets/plugins/basic_auth.py
+++ detect_secrets/plugins/basic_auth.py
@@ -2,38 +2,20 @@ from __future__ import absolute_import
 
 import re
 
-from .base import BasePlugin
-from detect_secrets.core.potential_secret import PotentialSecret
+from .base import RegexBasedDetector
 
 
 SPECIAL_URL_CHARACTERS = ':/?#[]@'
-BASIC_AUTH_REGEX = re.compile(
-    r'://[^{}\s]+:([^{}\s]+)@'.format(
-        re.escape(SPECIAL_URL_CHARACTERS),
-        re.escape(SPECIAL_URL_CHARACTERS),
-    ),
-)
 
 
-class BasicAuthDetector(BasePlugin):
+class BasicAuthDetector(RegexBasedDetector):
 
     secret_type = 'Basic Auth Credentials'
-
-    def analyze_string(self, string, line_num, filename):
-        output = {}
-
-        for result in self.secret_generator(string):
-            secret = PotentialSecret(
-                self.secret_type,
-                filename,
-                result,
-                line_num,
-            )
-            output[secret] = secret
-
-        return output
-
-    def secret_generator(self, string):
-        results = BASIC_AUTH_REGEX.findall(string)
-        for result in results:
-            yield result
+    blacklist = [
+        re.compile(
+            r'://[^{}\s]+:([^{}\s]+)@'.format(
+                re.escape(SPECIAL_URL_CHARACTERS),
+                re.escape(SPECIAL_URL_CHARACTERS),
+            ),
+        ),
+    ]
