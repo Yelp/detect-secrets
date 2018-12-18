8ad4923b341adf99d3b91f307d05fca64d93ce29
Joshua Li <joshuali@yelp.com>
2018-12-17 16:25:11 -0800

    refactor PrivateKeyDetector to use RegexBasedDetector

--- detect_secrets/plugins/private_key.py
+++ detect_secrets/plugins/private_key.py
@@ -1,68 +1,25 @@
-"""
-This code was extracted in part from
-https://github.com/pre-commit/pre-commit-hooks. Using similar heuristic logic,
-we adapted it to fit our plugin infrastructure, to create an organized,
-concerted effort in detecting all type of secrets in code.
-
-Copyright (c) 2014 pre-commit dev team: Anthony Sottile, Ken Struys
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
 from __future__ import absolute_import
 
-from .base import BasePlugin
-from detect_secrets.core.potential_secret import PotentialSecret
+import re
+
+from .base import RegexBasedDetector
 
 
-BLACKLIST = (
-    'BEGIN RSA PRIVATE KEY',
-    'BEGIN DSA PRIVATE KEY',
-    'BEGIN EC PRIVATE KEY',
-    'BEGIN OPENSSH PRIVATE KEY',
-    'BEGIN PRIVATE KEY',
-    'PuTTY-User-Key-File-2',
-    'BEGIN SSH2 ENCRYPTED PRIVATE KEY',
-)
-
-
-class PrivateKeyDetector(BasePlugin):
+class PrivateKeyDetector(RegexBasedDetector):
     """This checks for private keys by determining whether the blacklisted
     lines are present in the analyzed string.
     """
 
     secret_type = 'Private Key'
-
-    def analyze_string(self, string, line_num, filename):
-        output = {}
-
-        for identifier in self.secret_generator(string):
-            secret = PotentialSecret(
-                self.secret_type,
-                filename,
-                identifier,
-                line_num,
-            )
-            output[secret] = secret
-
-        return output
-
-    def secret_generator(self, string):
-        for line in BLACKLIST:
-            if line in string:
-                yield line
+    blacklist = [
+        re.compile(regexp)
+        for regexp in (
+            r'BEGIN RSA PRIVATE KEY',
+            r'BEGIN DSA PRIVATE KEY',
+            r'BEGIN EC PRIVATE KEY',
+            r'BEGIN OPENSSH PRIVATE KEY',
+            r'BEGIN PRIVATE KEY',
+            r'PuTTY-User-Key-File-2',
+            r'BEGIN SSH2 ENCRYPTED PRIVATE KEY',
+        )
+    ]
