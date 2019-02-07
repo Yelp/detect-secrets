ffe3d76bdb3e936f1dd7e5084b0b118a0cf47fde
Aaron Loo <aaronloo@yelp.com>
2019-02-07 15:22:32 -0800

    Adding more delimiters to BasicAuthDetector

--- detect_secrets/plugins/basic_auth.py
+++ detect_secrets/plugins/basic_auth.py
@@ -5,8 +5,13 @@ import re
 from .base import RegexBasedDetector
 
 
+# This list is derived from RFC 3986 Section 2.2.
+#
+# We do not expect any of these delimiter characters to appear in
+# the username/password component of the URL, seeing that this would probably
+# result in an unexpected URL parsing (and probably won't even work).
 RESERVED_CHARACTERS = ':/?#[]@'
-SUB_DELIMITER_CHARACTERS = '!$&\';'  # and anything else we might need
+SUB_DELIMITER_CHARACTERS = '!$&\'()*+,;='
 
 
 class BasicAuthDetector(RegexBasedDetector):
