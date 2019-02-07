7e43b9164db6b7bc018eafec52585c8151f07940
Xianjun Zhu <xianjun@us.ibm.com>
2019-02-06 20:26:32 -0500

    feat: relax check on whitelist string

--- detect_secrets/plugins/common/constants.py
+++ detect_secrets/plugins/common/constants.py
@@ -4,11 +4,11 @@ import re
 WHITELIST_REGEXES = [
     re.compile(r)
     for r in [
-        r'[ \t]+{} ?pragma: ?whitelist[ -]secret{}[ \t]*$'.format(start, end)
+        r'[ \t]+{} *pragma: ?whitelist[ -]secret{}[ \t]*$'.format(start, end)
         for start, end in (
             ('#', ''),              # e.g. python or yaml
             ('//', ''),             # e.g. golang
-            (r'/\*', r' ?\*/'),     # e.g. c
+            (r'/\*', r' *\*/'),     # e.g. c
             ('\'', ''),             # e.g. visual basic .net
             ('--', ''),             # e.g. sql
             # many other inline comment syntaxes are not included,
