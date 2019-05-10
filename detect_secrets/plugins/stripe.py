936efc98c59b4c86b02b99653cbb52eaa546f383
Daniel González Lopes <danielgonzalezlopes@gmail.com>
2019-05-10 09:58:29 +0000

    Fix pep8 style
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>

--- detect_secrets/plugins/stripe.py
+++ detect_secrets/plugins/stripe.py
@@ -13,6 +13,6 @@ class StripeDetector(RegexBasedDetector):
     secret_type = 'Stripe Access Key'
 
     blacklist = (
-            # stripe standard keys begin with sk_live and restricted with rk_live
-            re.compile(r'(r|s)k_live_[0-9a-zA-Z]{24}'),
+        # stripe standard keys begin with sk_live and restricted with rk_live
+        re.compile(r'(r|s)k_live_[0-9a-zA-Z]{24}'),
     )
