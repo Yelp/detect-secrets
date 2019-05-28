7751bbe673acc117afee75da4b905a7ec439c48b
Aaron Loo <domanchi@users.noreply.github.com>
2019-05-28 09:08:46 -0700

    adding ability to choose plugins in benchmark script

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import argparse
 import json
 import subprocess
@@ -9,28 +9,43 @@ from detect_secrets.core.usage import PluginOptions
 
 
 def main():
-    flag_list = []
     args = get_arguments()
 
-    # Fill flags list with PluginOptions flag text
-    for flag_number, flag in enumerate(PluginOptions.all_plugins):
-        flag_list.append(flag.disable_flag_text)
+    # First, convert chosen plugins into their disabled flags
+    always_disabled_plugins = []
+    flag_list = []
+    for info in PluginOptions.all_plugins:
+        if info.classname in args.plugin:
+            flag_list.append(info.disable_flag_text)
+        else:
+            always_disabled_plugins.append(info.disable_flag_text)
 
-    # Run benchmarks for all the cases
-    timings = {
-        'all-plugins': time_execution([], args.filenames),
-    }
+    # Then, iterate through each disabled flag, toggling them off
+    # individually.
+    timings = {}
+    if len(args.plugin) == len(PluginOptions.all_plugins):
+        # Only run benchmarks for all the cases, if already running all plugins
+        timings['all-plugins'] = time_execution([], args.filenames)
+
     for flag_number, flag in enumerate(flag_list):
         plugins_to_ignore = list(flag_list)
         plugins_to_ignore.pop(flag_number)
 
         key = flag[len('--no-'):-len('-scan')]
-        timings[key] = time_execution(plugins_to_ignore, args.filenames)
+        timings[key] = time_execution(
+            plugins_to_ignore + always_disabled_plugins,
+            args.filenames,
+        )
 
     print_output(timings, args)
 
 
 def get_arguments():
+    plugins = [
+        info.classname
+        for info in PluginOptions.all_plugins
+    ]
+
     parser = argparse.ArgumentParser(description='Run some benchmarks.')
     parser.add_argument(
         'filenames',
@@ -42,10 +57,23 @@ def get_arguments():
         action='store_true',
         help='Human readable output.',
     )
+    parser.add_argument(
+        '--plugin',
+        default=None,       # needs to be None, otherwise append won't work as expected
+        choices=plugins,
+        action='append',
+        help=(
+            'Specifies a plugin to test. May provide multiple values. '
+            'Defaults to all.'
+        ),
+    )
 
     args = parser.parse_args()
     if not args.filenames:
         args.filenames = ['../.']
+
+    if not args.plugin:
+        args.plugin = plugins
 
     return args
 
