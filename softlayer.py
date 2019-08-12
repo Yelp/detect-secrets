efff32e4f2ccd362c70f8b120a1486e3cadcb5b5
Justin D. Eyster <Justin.Eyster@ibm.com>
2019-08-12 13:38:36 -0400

    Strengthen SoftLayer regex by including "password" keywords (#171)
    * Strengthen SoftLayer regex by including "password" keywords
    
    Noticed the legacy crawler searched for the words `password`, `pwd`, `pass` in addition to `key`. Adapted our solution to follow.
    
    * Var name update
    
    * Add additional assign operator from crawler, too

--- softlayer.py
+++ softlayer.py
@@ -15,20 +15,20 @@ class SoftLayerDetector(RegexBasedDetector):
     sl = r'(?:softlayer|sl)'
     opt_dash_undrscr = r'(?:_|-|)'
     opt_api = r'(?:api|)'
-    key = r'key'
+    key_or_pass = r'(?:key|pwd|password|pass)'
     opt_space = r'(?: |)'
-    opt_equals = r'(?:=|:|:=|)'
+    opt_equals = r'(?:=|:|:=|=>|)'
     secret = r'([a-z0-9]{64})'
     denylist = [
         re.compile(
-            r'{opt_quote}{opt_dashes}{sl}{opt_dash_undrscr}{opt_api}{opt_dash_undrscr}{key}'
+            r'{opt_quote}{opt_dashes}{sl}{opt_dash_undrscr}{opt_api}{opt_dash_undrscr}{key_or_pass}'
             '{opt_quote}{opt_space}{opt_equals}{opt_space}{opt_quote}{secret}{opt_quote}'.format(
                 opt_quote=opt_quote,
                 opt_dashes=opt_dashes,
                 sl=sl,
                 opt_dash_undrscr=opt_dash_undrscr,
                 opt_api=opt_api,
-                key=key,
+                key_or_pass=key_or_pass,
                 opt_space=opt_space,
                 opt_equals=opt_equals,
                 secret=secret,
