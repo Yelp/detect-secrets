7052257d58fda9b2e11298abdf9adeb088d9f752
Pankaj Patil <pankaj.patil2099@hotmail.com>
2019-08-28 22:46:02 +0530

    Added Verifiability Slack

--- detect_secrets/plugins/slack.py
+++ detect_secrets/plugins/slack.py
@@ -27,13 +27,26 @@ class SlackDetector(RegexBasedDetector):
         ),
     )
 
-    def verify(self, token, **kwargs):    # pragma: no cover
-        response = requests.post(
-            'https://slack.com/api/auth.test',
-            data={
-                'token': token,
-            },
-        ).json()
+    def verify(self, token, **kwargs):  # pragma: no cover
+        if token.startswith('https://hooks.slack.com/services/T'):
+            response = requests.post(
+                token,
+                json={
+                    'text': '',
+                },
+            )
+            valid = response.text == "missing_text_or_fallback_or_attachments"
+        else:
+            response = requests.post(
+                'https://slack.com/api/auth.test',
+                data={
+                    'token': token,
+                },
+            ).json()
+            valid = response['ok']
 
-        return VerifiedResult.VERIFIED_TRUE if response['ok'] \
+        return (
+            VerifiedResult.VERIFIED_TRUE
+            if valid
             else VerifiedResult.VERIFIED_FALSE
+        )
