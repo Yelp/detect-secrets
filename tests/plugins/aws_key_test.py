6c6e028210261bb15745599db33622eb49a1c342
Kevin Hock <khock@yelp.com>
2019-09-20 21:07:37 -0700

    :mortar_board: Use plural variable name for list

--- tests/plugins/aws_key_test.py
+++ tests/plugins/aws_key_test.py
@@ -8,7 +8,7 @@ import pytest
 
 from detect_secrets.core.constants import VerifiedResult
 from detect_secrets.plugins.aws import AWSKeyDetector
-from detect_secrets.plugins.aws import get_secret_access_key
+from detect_secrets.plugins.aws import get_secret_access_keys
 from testing.mocks import mock_file_object
 
 
@@ -119,7 +119,7 @@ class TestAWSKeyDetector(object):
             [EXAMPLE_SECRET],
         ),
 
-        # multiple candidates
+        # Multiple candidates
         (
             textwrap.dedent("""
                 base64_keyA = '{}'
@@ -142,4 +142,4 @@ class TestAWSKeyDetector(object):
     ),
 )
 def test_get_secret_access_key(content, expected_output):
-    assert get_secret_access_key(content) == expected_output
+    assert get_secret_access_keys(content) == expected_output
