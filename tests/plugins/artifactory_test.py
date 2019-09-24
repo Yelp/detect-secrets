6d5d795a327764c945618d9a4260c5cb8e026d0a
Justin D. Eyster <Justin.Eyster@ibm.com>
2019-09-24 17:01:08 -0400

    Verify Artifactory catches expected secret (#198)
    * Verify Artifactory catches expected secret
    
    Follow up of [this comment](https://github.ibm.com/Whitewater/whitewater-detect-secrets/pull/197#discussion_r2457154) on #197.
    
    * Fix cannot index dict.keys object

--- tests/plugins/artifactory_test.py
+++ tests/plugins/artifactory_test.py
@@ -12,40 +12,42 @@ ARTIFACTORY_TOKEN_BYTES = b'AKCxxxxxxxxxx'
 class TestArtifactoryDetector(object):
 
     @pytest.mark.parametrize(
-        'payload, should_flag',
+        'token, payload, should_flag',
         [
-            ('AP6xxxxxxxxxx', True),
-            ('AP2xxxxxxxxxx', True),
-            ('AP3xxxxxxxxxx', True),
-            ('AP5xxxxxxxxxx', True),
-            ('APAxxxxxxxxxx', True),
-            ('APBxxxxxxxxxx', True),
-            ('AKCxxxxxxxxxx', True),
-            (' AP6xxxxxxxxxx', True),
-            (' AKCxxxxxxxxxx', True),
-            ('=AP6xxxxxxxxxx', True),
-            ('=AKCxxxxxxxxxx', True),
-            ('\"AP6xxxxxxxxxx\"', True),
-            ('\"AKCxxxxxxxxxx\"', True),
-            ('artif-key:AP6xxxxxxxxxx', True),
-            ('artif-key:AKCxxxxxxxxxx', True),
-            ('X-JFrog-Art-Api: AKCxxxxxxxxxx', True),
-            ('X-JFrog-Art-Api: AP6xxxxxxxxxx', True),
-            ('artifactoryx:_password=AKCxxxxxxxxxx', True),
-            ('artifactoryx:_password=AP6xxxxxxxxxx', True),
-            ('testAKCwithinsomeirrelevantstring', False),
-            ('testAP6withinsomeirrelevantstring', False),
-            ('X-JFrog-Art-Api: $API_KEY', False),
-            ('X-JFrog-Art-Api: $PASSWORD', False),
-            ('artifactory:_password=AP6xxxxxx', False),
-            ('artifactory:_password=AKCxxxxxxxx', False),
+            ('AP6xxxxxxxxxx', 'AP6xxxxxxxxxx', True),
+            ('AP2xxxxxxxxxx', 'AP2xxxxxxxxxx', True),
+            ('AP3xxxxxxxxxx', 'AP3xxxxxxxxxx', True),
+            ('AP5xxxxxxxxxx', 'AP5xxxxxxxxxx', True),
+            ('APAxxxxxxxxxx', 'APAxxxxxxxxxx', True),
+            ('APBxxxxxxxxxx', 'APBxxxxxxxxxx', True),
+            ('AKCxxxxxxxxxx', 'AKCxxxxxxxxxx', True),
+            ('AP6xxxxxxxxxx', ' AP6xxxxxxxxxx', True),
+            ('AKCxxxxxxxxxx', ' AKCxxxxxxxxxx', True),
+            ('AP6xxxxxxxxxx', '=AP6xxxxxxxxxx', True),
+            ('AKCxxxxxxxxxx', '=AKCxxxxxxxxxx', True),
+            ('AP6xxxxxxxxxx', '\"AP6xxxxxxxxxx\"', True),
+            ('AKCxxxxxxxxxx', '\"AKCxxxxxxxxxx\"', True),
+            ('AP6xxxxxxxxxx', 'artif-key:AP6xxxxxxxxxx', True),
+            ('AKCxxxxxxxxxx', 'artif-key:AKCxxxxxxxxxx', True),
+            ('AKCxxxxxxxxxx', 'X-JFrog-Art-Api: AKCxxxxxxxxxx', True),
+            ('AP6xxxxxxxxxx', 'X-JFrog-Art-Api: AP6xxxxxxxxxx', True),
+            ('AKCxxxxxxxxxx', 'artifactoryx:_password=AKCxxxxxxxxxx', True),
+            ('AP6xxxxxxxxxx', 'artifactoryx:_password=AP6xxxxxxxxxx', True),
+            ('', 'testAKCwithinsomeirrelevantstring', False),
+            ('', 'testAP6withinsomeirrelevantstring', False),
+            ('', 'X-JFrog-Art-Api: $API_KEY', False),
+            ('', 'X-JFrog-Art-Api: $PASSWORD', False),
+            ('', 'artifactory:_password=AP6xxxxxx', False),
+            ('', 'artifactory:_password=AKCxxxxxxxx', False),
         ],
     )
-    def test_analyze_line(self, payload, should_flag):
+    def test_analyze_string(self, token, payload, should_flag):
         logic = ArtifactoryDetector()
 
-        output = logic.analyze_line(payload, 1, 'mock_filename')
+        output = logic.analyze_string(payload, 1, 'mock_filename', output_raw=True)
         assert len(output) == int(should_flag)
+        if len(output) > 0:
+            assert list(output.keys())[0].secret == token
 
     @responses.activate
     def test_verify_invalid_secret(self):
