9e30f0064f2e481e8dc8f371a29630984082e4c8
Justin D. Eyster <Justin.Eyster@ibm.com>
2019-09-24 15:30:37 -0400

    Fix Artifactory regex to no longer match assignment character (#197)
    * Fix Artifactory regex to no longer match assignment character
    
    * Fix 'look-behind requires fixed-width pattern'

--- detect_secrets/plugins/artifactory.py
+++ detect_secrets/plugins/artifactory.py
@@ -11,10 +11,10 @@ class ArtifactoryDetector(RegexBasedDetector):
     secret_type = 'Artifactory Credentials'
 
     denylist = [
-        # Artifactory tokens begin with AKC
-        re.compile(r'(?:\s|=|:|"|^)AKC[a-zA-Z0-9]{10,}'),  # API token
-        # Artifactory encrypted passwords begin with AP[A-Z]
-        re.compile(r'(?:\s|=|:|"|^)AP[\dABCDEF][a-zA-Z0-9]{8,}'),  # Password
+        # artifactory tokens begin with AKC
+        re.compile(r'(?:(?<==|:|")|(?<=\s)|(?<=^))AKC[a-zA-Z0-9]{10,}'),    # api token
+        # artifactory encrypted passwords begin with AP[A-Z]
+        re.compile(r'(?:(?<==|:|")|(?<=\s)|(?<=^))AP[\dABCDEF][a-zA-Z0-9]{8,}'),    # password
     ]
 
     artifactory_url = 'na.artifactory.swg-devops.com/artifactory'
