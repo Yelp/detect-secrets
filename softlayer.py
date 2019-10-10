da3d9cae678a9b39f5974acde3b4c2db0a5e211d
XIANJUN ZHU <xianjun@us.ibm.com>
2019-10-10 16:31:44 -0400

    Use RegexBasedDetector.assign_regex_generator (#215)
    * feat: use assign regex in cloudant
    
    * feat: use assign regex in db2
    
    * feat: use assign regex in gh
    
    * feat: use assign regex in iam
    
    * feat: use assign regex in sl
    
    * address comments
    
    * address comments
    
    * address comments

--- softlayer.py
+++ softlayer.py
@@ -13,31 +13,16 @@ class SoftLayerDetector(RegexBasedDetector):
     secret_type = 'SoftLayer Credentials'
 
     # opt means optional
-    opt_quote = r'(?:"|\'|)'
-    opt_dashes = r'(?:--|)'
-    sl = r'(?:softlayer|sl)'
-    opt_dash_undrscr = r'(?:_|-|)'
-    opt_api = r'(?:api|)'
+    sl = r'(?:softlayer|sl)(?:_|-|)(?:api|)'
     key_or_pass = r'(?:key|pwd|password|pass|token)'
-    opt_space = r'(?: *)'
-    opt_assignment = r'(?:=|:|:=|=>|)'
     secret = r'([a-z0-9]{64})'
     denylist = [
-        re.compile(
-            r'{opt_quote}{opt_dashes}{sl}{opt_dash_undrscr}{opt_api}{opt_dash_undrscr}{key_or_pass}'
-            '{opt_quote}{opt_space}{opt_assignment}{opt_space}{opt_quote}{secret}'
-            '{opt_quote}'.format(
-                opt_quote=opt_quote,
-                opt_dashes=opt_dashes,
-                sl=sl,
-                opt_dash_undrscr=opt_dash_undrscr,
-                opt_api=opt_api,
-                key_or_pass=key_or_pass,
-                opt_space=opt_space,
-                opt_assignment=opt_assignment,
-                secret=secret,
-            ), flags=re.IGNORECASE,
+        RegexBasedDetector.assign_regex_generator(
+            prefix_regex=sl,
+            password_keyword_regex=key_or_pass,
+            password_regex=secret,
         ),
+
         re.compile(
             r'(?:http|https)://api.softlayer.com/soap/(?:v3|v3.1)/([a-z0-9]{64})',
             flags=re.IGNORECASE,
@@ -45,7 +30,7 @@ class SoftLayerDetector(RegexBasedDetector):
     ]
 
     def verify(self, token, content, potential_secret=None):
-        usernames = get_username(content)
+        usernames = find_username(content)
         if not usernames:
             return VerifiedResult.UNVERIFIED
 
@@ -55,30 +40,17 @@ class SoftLayerDetector(RegexBasedDetector):
         return VerifiedResult.VERIFIED_FALSE
 
 
-def get_username(content):
+def find_username(content):
     # opt means optional
-    opt_quote = r'(?:"|\'|)'
-    opt_dashes = r'(?:--|)'
-    opt_sl = r'(?:softlayer|sl|)'
-    opt_dash_undrscr = r'(?:_|-|)'
-    opt_api = r'(?:api|)'
-    username_keyword = r'(?:username|id|user|userid|user-id|user-name|name|user_id|user_name|uname)'
-    opt_space = r'(?: |)'
-    seperator = r'(?: |=|:|:=|=>)+'
+    username_keyword = r'(?:username|id|user|userid|user-id|user-name|' + \
+        r'name|user_id|user_name|uname)'
     username = r'(\w(?:\w|_|@|\.|-)+)'
     regex = re.compile(
-        r'{opt_quote}{opt_dashes}{opt_sl}{opt_dash_undrscr}{opt_api}{opt_dash_undrscr}'
-        '{username_keyword}{opt_quote}{seperator}{opt_quote}{username}{opt_quote}'.format(
-            opt_quote=opt_quote,
-            opt_dashes=opt_dashes,
-            opt_sl=opt_sl,
-            opt_dash_undrscr=opt_dash_undrscr,
-            opt_api=opt_api,
-            username_keyword=username_keyword,
-            opt_space=opt_space,
-            username=username,
-            seperator=seperator,
-        ), flags=re.IGNORECASE,
+        RegexBasedDetector.assign_regex_generator(
+            prefix_regex=SoftLayerDetector.sl,
+            password_keyword_regex=username_keyword,
+            password_regex=username,
+        ),
     )
 
     return [
