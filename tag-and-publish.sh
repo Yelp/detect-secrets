e4914f71b262a8fb715b0ee0bc174f89ea4498f3
XIANJUN ZHU <xianjun@us.ibm.com>
2020-02-18 16:49:54 -0500

    Publish to Artifactory (#250)
    * Publish art too
    
    * Dummy... not echo, but action

--- tag-and-publish.sh
+++ tag-and-publish.sh
@@ -2,13 +2,24 @@
 
 script_name=$(basename $0)
 
-if [ $# -lt 2 ]; then
-    echo "Usage: $script_name local_image_with_tag remote_image_with_tag"
+if [ $# -lt 4 ]; then
+    echo "Usage: $script_name image_names local_domain_name remote_domain_names tags"
     exit 1
 fi
 
-LOCAL_IMAGE=$1
-REMOTE_IAMGE=$2
+DOCKER_IMAGES=$1
+DOCKER_DOMAIN_LOCAL=$2
+DOCKER_DOMAIN_REMOTES=$3
+TAG_NAMES=$4
 
-docker tag $LOCAL_IMAGE $REMOTE_IAMGE
-docker push $REMOTE_IAMGE
+for image_name in $DOCKER_IMAGES
+do
+    for domain_name in $DOCKER_DOMAIN_REMOTES
+    do
+        for tag in $TAG_NAMES
+        do
+            docker tag $DOCKER_DOMAIN_LOCAL/$image_name $domain_name/$image_name:$tag
+            docker push $domain_name/$image_name:$tag
+        done
+    done
+done
