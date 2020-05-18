a522564e2766961b864056894fe40804b99808f9
XIANJUN ZHU <xianjun@us.ibm.com>
2020-05-18 17:09:45 -0400

    Add back missing param (#314)

--- detect_secrets/plugins/base.py
+++ detect_secrets/plugins/base.py
@@ -224,7 +224,7 @@ class BasePlugin:
 
         verified_result = VerifiedResult.UNVERIFIED
         for result in results:
-            is_verified = self.verify(result.secret_value)
+            is_verified = self.verify(result.secret_value, string, result)
             if is_verified != VerifiedResult.UNVERIFIED:
                 verified_result = is_verified
                 break
