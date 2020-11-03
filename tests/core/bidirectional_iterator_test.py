6783cb896ec70e74f76afbfd3240b2c2767b795a
Aaron Loo <aaronloo@yelp.com>
2020-11-03 08:50:27 -0800

    moving detect_secrets.core.bidirectional_iterator to detect_secrets.audit.bidirectional_iterator

--- tests/core/bidirectional_iterator_test.py
+++ tests/core/bidirectional_iterator_test.py
@@ -1,23 +1,23 @@
 import pytest
 
-from detect_secrets.core import bidirectional_iterator
+from detect_secrets.audit.bidirectional_iterator import BidirectionalIterator
 
 
 class TestBidirectionalIterator:
 
     def test_no_input(self):
-        iterator = bidirectional_iterator.BidirectionalIterator([])
+        iterator = BidirectionalIterator([])
         with pytest.raises(StopIteration):
             iterator.__next__()
 
     def test_cannot_step_back_too_far(self):
-        iterator = bidirectional_iterator.BidirectionalIterator([0])
+        iterator = BidirectionalIterator([0])
         iterator.step_back_on_next_iteration()
         with pytest.raises(StopIteration):
             iterator.__next__()
 
     def test_cannot_step_back_too_far_after_stepping_in(self):
-        iterator = bidirectional_iterator.BidirectionalIterator([0, 1, 2])
+        iterator = BidirectionalIterator([0, 1, 2])
         for _ in range(3):
             iterator.__next__()
         for _ in range(2):
@@ -28,7 +28,7 @@ class TestBidirectionalIterator:
             iterator.__next__()
 
     def test_works_correctly_in_loop(self):
-        iterator = bidirectional_iterator.BidirectionalIterator([0, 1, 2, 3, 4, 5])
+        iterator = BidirectionalIterator([0, 1, 2, 3, 4, 5])
         commands = [0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
         command_count = 0
         results = []
@@ -41,14 +41,14 @@ class TestBidirectionalIterator:
 
     def test_normal_iterator_if_not_told_to_step_back(self):
         input_list = [0, 1, 2, 3, 4, 5]
-        iterator = bidirectional_iterator.BidirectionalIterator(input_list)
+        iterator = BidirectionalIterator(input_list)
         results = []
         for index in iterator:
             results.append(index)
         assert results == input_list
 
     def test_knows_when_stepping_back_possible(self):
-        iterator = bidirectional_iterator.BidirectionalIterator([0, 1, 2, 3])
+        iterator = BidirectionalIterator([0, 1, 2, 3])
         commands = [0, 1, 0, 0, 1, 1, 0, 0, 0, 0]
         command_count = 0
         results = []
