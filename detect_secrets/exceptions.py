e66eb0b33f18e40fc5af210b1e5b0f280c72a9df
Aaron Loo <aaronloo@yelp.com>
2020-11-13 08:43:03 -0800

    whoops, the actual exceptions file

---
+++ detect_secrets/exceptions.py
@@ -0,0 +1,16 @@
+class UnableToReadBaselineError(ValueError):
+    """Think of this as a 404, if getting a baseline had a HTTPError code."""
+    pass
+
+
+class InvalidBaselineError(ValueError):
+    """Think of this as a 400, if getting a baseline had a HTTPError code."""
+    pass
+
+
+class SecretNotFoundOnSpecifiedLineError(Exception):
+    def __init__(self, line):
+        super(SecretNotFoundOnSpecifiedLineError, self).__init__(
+            'ERROR: Secret not found on line {}!\n'.format(line)
+            + 'Try recreating your baseline to fix this issue.',
+        )
