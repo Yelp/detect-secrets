a219fb6909fbaf50ed56cfa46a7583946c6d8290
Aaron Loo <aaronloo@yelp.com>
2021-01-13 18:24:47 -0800

    adding detect_secrets.settings.default_settings

--- tests/core/usage/scan_usage_test.py
+++ tests/core/usage/scan_usage_test.py
@@ -3,6 +3,7 @@ import tempfile
 
 import pytest
 
+from detect_secrets.core import plugins
 from detect_secrets.core.plugins.util import get_mapping_from_secret_type_to_class
 from detect_secrets.core.usage import ParserBuilder
 from detect_secrets.settings import get_settings
@@ -31,3 +32,11 @@ def test_force_use_all_plugins(parser):
         parser.parse_args(['scan', '--force-use-all-plugins', '--baseline', f.name])
 
     assert len(get_settings().plugins) == len(get_mapping_from_secret_type_to_class())
+
+
+def test_default_plugins_initialized(parser):
+    parser.parse_args(['scan', '--hex-limit', '2'])
+
+    assert len(get_settings().plugins) == len(get_mapping_from_secret_type_to_class())
+    assert plugins.initialize.from_plugin_classname('HexHighEntropyString').entropy_limit == 2
+    assert plugins.initialize.from_plugin_classname('Base64HighEntropyString').entropy_limit == 4.5
