579c9cc207a1a0e75c4346e19f670ef1cc27b9b3
Aaron Loo <aaronloo@yelp.com>
2021-04-08 08:54:13 -0700

    bug fix: upgrading from baseline.exclude should apply to files, not lines

--- detect_secrets/core/upgrades/v0_12.py
+++ detect_secrets/core/upgrades/v0_12.py
@@ -5,8 +5,8 @@ from typing import Dict
 def upgrade(baseline: Dict[str, Any]) -> None:
     if 'exclude_regex' in baseline:
         baseline['exclude'] = {
-            'files': None,
-            'lines': baseline.pop('exclude_regex'),
+            'files': baseline.pop('exclude_regex'),
+            'lines': None,
         }
 
     baseline['word_list'] = {
