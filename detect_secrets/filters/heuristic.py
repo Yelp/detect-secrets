4ceff679333787bdd551f2e779054553b4bbf536
John-Paul Dakran <dakranj@yelp.com>
2022-02-07 17:13:07 -0500

    Document Design Rationales for Heuristic Constraint (#516)
    * Add comment for rationale of heurisitic constraint
    
    * Update comment

--- detect_secrets/filters/heuristic.py
+++ detect_secrets/filters/heuristic.py
@@ -165,6 +165,8 @@ def is_indirect_reference(line: str) -> bool:
 
         secret = request.headers['apikey']
     """
+    # Constrain line length as the heuristic's intention is to target lines that resemble
+    # function calls. The constraint avoids catastrophic backtracking failures of the regex.
     if len(line) > 1000:
         return False
     return bool(_get_indirect_reference_regex().search(line))
