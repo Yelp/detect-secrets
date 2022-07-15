ebb1fca7d6a7e324db09d98928cbfddc5a84c79f
Abdel Thafeer <abed@yelp.com>
2022-07-15 07:34:00 -0700

    add test data to check if macos will fail tests

--- test_data/files/file_with_custom_secrets.py
+++ test_data/files/file_with_custom_secrets.py
@@ -2,3 +2,5 @@
 REGULAR_VALUE = 'this is just a long string, like a user facing error message'
 FOOD = 'sweet potato casserole'
 ANIMAL = 'Hippo'
+CL_ACCOUNT = 'testy_-test'
+CL_PW = 'abcd1234abcd1234abcd1234abcd1234abcd1234abcd1234abcd1234abcd1234'
