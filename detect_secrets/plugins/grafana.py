5246602dc6b8e2372bbe7559644d62722185e299
chagrafana <charline@Charlines-MacBook-Pro.local>
2022-08-18 20:52:30 +0200

    Removing commented regex for Grafana Cloud API Token & Grafana Api Key

--- detect_secrets/plugins/grafana.py
+++ detect_secrets/plugins/grafana.py
@@ -10,11 +10,12 @@ class GrafanaDetector(RegexBasedDetector):
     secret_type = 'Grafana Token'
 
     denylist = [
-        # Grafana Cloud API Token
-        #re.compile(r'glc_[A-Za-z0-9+/]{32,}={0,2}'),
+        # Grafana Cloud API Token & Grafana API Key
+        # Merging the two as there is an issue in the engine which will false-positive any 
+        # Grafana Cloud API Token as the actual data resembles the Grafana API Key
+        # e.g. contains eyJrIjoi, but does not match the rest of the pattern
         re.compile(r'glc_[A-Za-z0-9+/]{32,}={0,2}|eyJrIjoi[A-Za-z0-9]{70,}={0,2}'),
         # Grafana Service Account Token
         re.compile(r'glsa_[A-Za-z0-9]{32}_[A-Fa-f0-9]{8}'),
         # Grafana API Key
-        #re.compile(r'eyJrIjoi[A-Za-z0-9]{70,}={0,2}'),
     ]
