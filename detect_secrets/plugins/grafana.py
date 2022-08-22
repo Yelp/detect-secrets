a0480d254c1dab53c44bb56367d16922fb895e99
chagrafana <charline.voinot@grafana.com>
2022-08-22 18:09:49 +0200

    Fixing flake8 issues

--- detect_secrets/plugins/grafana.py
+++ detect_secrets/plugins/grafana.py
@@ -5,6 +5,7 @@ import re
 
 from .base import RegexBasedDetector
 
+
 class GrafanaDetector(RegexBasedDetector):
     """Scans for BGrafana tokens"""
     secret_type = 'Grafana Token'
@@ -17,5 +18,4 @@ class GrafanaDetector(RegexBasedDetector):
         re.compile(r'glc_[A-Za-z0-9+/]{32,}={0,2}|eyJrIjoi[A-Za-z0-9]{70,}={0,2}'),
         # Grafana Service Account Token
         re.compile(r'glsa_[A-Za-z0-9]{32}_[A-Fa-f0-9]{8}'),
-        # Grafana API Key
     ]
--- tests/plugins/grafana_test.py
+++ tests/plugins/grafana_test.py
@@ -8,10 +8,13 @@ class TestGrafanaDetector:
     @pytest.mark.parametrize(
         'payload, should_flag',
         [
-            ('eyJrIjoiR0ZXZmt1UFc0OEpIOGN5RWdUalBJTllUTk83VlhtVGwiLCJuIjoiYXBpa2V5Y3VybCIsImlkIjo2fQ==', True),
+            ('eyJrIjoiR0ZXZmt1UFc0OEpIOGN5RWdUalBJTllUTk83VlhtVGwiLCJuIjoiYXBpa2V5Y3VybCIsImlkIjo2'
+            'fQ==', True),
             ('glsa_Q0tTx59W5f7LXgxiM4MjJs75176oun5F_e5c6b96d', True),
-            ('eyJrIjoiZjJlZThiNDMwZGQ0ZDhjNjJhYmQwZWY3NDgxYzhjNjRiNzZkNjYyMiIsIm4iOiJ0ZXN0LWtleSIsImlkIjo5OTk5OX0=', True),
-            ('glc_eyJrIjoiZjI0YzZkNGEwZDBmZmZjMmUzNTU2ODcxMmY0ZWZlNTQ1NTljMDFjOCIsIm4iOiJteXRva2VuIiwiaWQiOjF9', True),
+            ('eyJrIjoiZjJlZThiNDMwZGQ0ZDhjNjJhYmQwZWY3NDgxYzhjNjRiNzZkNjYyMiIsIm4iOiJ0ZXN0LWtleSIs'
+            'ImlkIjo5OTk5OX0=', True),
+            ('glc_eyJrIjoiZjI0YzZkNGEwZDBmZmZjMmUzNTU2ODcxMmY0ZWZlNTQ1NTljMDFjOCIsIm4iOiJteXRva2Vu'
+            'IiwiaWQiOjF9', True),
         ],
     )
     def test_analyze(self, payload, should_flag):
