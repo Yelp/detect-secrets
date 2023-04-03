fc86aa615a22950ea744a0938a0c83d502fd48ec
Victoria Miltcheva <12804086+victoria-miltcheva@users.noreply.github.com>
2023-04-03 16:50:30 -0500

    Try to fix build
    Signed-off-by: Victoria Miltcheva <12804086+victoria-miltcheva@users.noreply.github.com>

--- scripts/tox/install_pre_commit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ "$SKIP_PRE_COMMIT" == true ]; then echo "Skipping pre-commit installation."
-else
-    pre-commit install -f --install-hooks
-fi
--- scripts/tox/run_pre_commit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ "$SKIP_PRE_COMMIT" == true ]; then echo "Skipping pre-commit run."
-else
-    pre-commit run --all-files --show-diff-on-failure
-fi
