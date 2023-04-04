4f190116f4e5d8c5630c0601c3e6d19dddf692fb
Eliran Turgeman <50831652+Eliran-Turgeman@users.noreply.github.com>
2023-04-04 15:31:26 +0300

    Fix azure storage key regex (#99)
    Co-authored-by: Eliran Turgeman <elturgeman@paloaltonetworks.com>

--- tests/plugins/azure_storage_key_test.py
+++ tests/plugins/azure_storage_key_test.py
@@ -12,6 +12,10 @@ class TestAzureStorageKeyDetector:
                 'AccountKey=lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==',  # noqa: E501
                 True,
             ),
+            (
+                'lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==',  # noqa: E501
+                True,
+            ),
         ],
     )
     def test_analyze(self, payload, should_flag):
