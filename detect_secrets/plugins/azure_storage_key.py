19d606c34406d20d0164a2f3e10e5c4865921b84
Eliran Turgeman <50831652+Eliran-Turgeman@users.noreply.github.com>
2023-04-05 10:14:22 +0300

    Revert regex of azure storage key (#101)

--- detect_secrets/plugins/azure_storage_key.py
+++ detect_secrets/plugins/azure_storage_key.py
@@ -13,6 +13,6 @@ class AzureStorageKeyDetector(RegexBasedDetector):
     denylist = [
         # Account Key (AccountKey=xxxxxxxxx)
         re.compile(
-            r'(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{4})$',
+            r'AccountKey=[a-zA-Z0-9+\/=]{88}',
         ),
     ]
