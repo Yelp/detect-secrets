2fed4a858e5696814ab55d9d03edfe409d2debb3
StuffyMonkey <n.artemenko.work@gmail.com>
2023-12-13 21:52:09 +0700

    Update telegram_token_test.py

--- tests/plugins/telegram_token_test.py
+++ tests/plugins/telegram_token_test.py
@@ -5,8 +5,8 @@ class TestTelegramTokenDetector:
     @pytest.mark.parametrize(
         'payload, should_flag',
         [
-            ('6751529203:AAE17nfyMEszRf4KrOXotArH4HzTt2opO2s', True),
-            ('6151527a03:AAE17nfyMEszRf4KrOXotArH4HzTt2opO2s', False),
+            ('6741529203:AAE17nfyMEuzRf4LrOXokArH4HzTt2opO2s', True),
+            ('6151527a03:AAE17nfrMEfzRf4KrOXotArH4HzTt2opO2s', False),
             ('6910343172:AAGiAHt7SPe_rryV9AVK48-IBisa-Zq-azc', True),
             ('6990343172:AAGiAHt7SPe~rryV9AVK48-IBisa-Zq-azc', False),
             ('database', False),
