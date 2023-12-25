04e582d1d6f00b1392871f36d4a5fd3cb963915c
pazbec <paz8097@gmail.com>
2023-12-25 14:52:26 +0200

    Fix bug (#173)
    Co-authored-by: pazbechor <pbechor@paloaltonetworks.com>

--- detect_secrets/plugins/keyword.py
+++ detect_secrets/plugins/keyword.py
@@ -97,7 +97,9 @@ QUOTE = r'[\'"`]'
 #   [^\v,\'"`]  ->  this section match with the last secret character that can be everything except
 #                   line breaks, comma, backticks or quotes. This allows to reduce the false
 #                   positives number and to prevent errors in the code snippet highlighting.
-SECRET = r'(?=[^\v\'\"]*)(?=\w+)[^\v\'\"]*[^\v,\'\"`]'
+
+# '$' sign is being used for variable rendering. That why it's not here.
+SECRET = r'(?=[^\v\'\"]*)(?=[a-zA-Z0-9_!@#%^&])[^\v\'\"]*[^\v,\'\"`]'
 SQUARE_BRACKETS = r'(\[[0-9]*\])'
 
 FOLLOWED_BY_COLON_EQUAL_SIGNS_REGEX = re.compile(
@@ -122,6 +124,7 @@ FOLLOWED_BY_COLON_REGEX = re.compile(
     ),
     flags=re.IGNORECASE,
 )
+
 FOLLOWED_BY_COLON_QUOTES_REQUIRED_REGEX = re.compile(
     # e.g. api_key: "foo"
     r'{denylist}({closing})?:({whitespace})({quote})({secret})(\4)'.format(
