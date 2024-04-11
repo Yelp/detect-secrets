cbca658188e36948a0e31bef834c714fddcd87c6
Anatolii Stadnichuk <astadnichuk@paloaltonetworks.com>
2024-04-11 12:40:43 +0100

    AzureStorageKeyDetector should flag a secret in two scenarios:
    `AccountKey=` is before the key
    
    azure (case insensitive) is mentioned within the context of the secret (within the surrounding 2 lines)

--- tests/plugins/azure_storage_key_test.py
+++ tests/plugins/azure_storage_key_test.py
@@ -22,7 +22,7 @@ class TestAzureStorageKeyDetector:
             ),
             (
                 'lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==',  # noqa: E501
-                True,
+                False,
             ),
             (
                 'learS3Bucket',
@@ -48,7 +48,7 @@ class TestAzureStorageKeyDetector:
             ),
             (
                 'SshPublicKey: ssh-rsa lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==',
-                    False,
+                False,
             ),
             (
                 'PublicKeys: lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==',
@@ -59,15 +59,83 @@ class TestAzureStorageKeyDetector:
                 False,
             ),
             (
-                'PrivateKeys: lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==',
+                '(https://portal.azure.com/). +CREATE DATABASE SCOPED CREDENTIAL AzureStorageCredential +WITH IDENTITY = \'IDENTITY\', lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==',
                 True,
             ),
 
             # Test multilines
+
+            # azure is mentioned within the 1 line above
             (
-                """PrivateKeys:
-                - lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==""",
+                """(https://portal.azure.com/). +CREATE DATABASE SCOPED CREDENTIAL AzureStorageCredential +WITH IDENTITY = 'IDENTITY',
+                lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==""",
                 True,
+            ),
+            # azure is mentioned within the 2 lines above
+            (
+                """(https://portal.azure.com/). +CREATE DATABASE SCOPED CREDENTIAL AzureStorageCredential +WITH IDENTITY = 'IDENTITY',
+                some data here
+                lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==""",
+                True,
+            ),
+            # azure is mentioned within the 2 lines above
+            (
+                """(https://portal.azure.com/). +CREATE DATABASE SCOPED CREDENTIAL AzureStorageCredential +WITH IDENTITY = 'IDENTITY',
+
+                lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==""",
+                True,
+            ),
+            # azure is mentioned within the 3 lines above
+            (
+                """(https://portal.azure.com/). +CREATE DATABASE SCOPED CREDENTIAL AzureStorageCredential +WITH IDENTITY = 'IDENTITY',
+                some data 1
+                some data 2
+                lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==""",
+                False,
+            ),
+            # azure lowercase
+            (
+                """azure.com +WITH IDENTITY = 'IDENTITY',
+                lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==""",
+                True,
+            ),
+            # azure uppercase
+            (
+                """AZURE.com +WITH IDENTITY = 'IDENTITY',
+                lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==""",
+                True,
+            ),
+            # azure capitalize
+            (
+                """Azure.com +WITH IDENTITY = 'IDENTITY',
+                lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==""",
+                True,
+            ),
+            # azure is mentioned within the 1 line below
+            (
+                """lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==
+                (https://portal.azure.com/). +CREATE DATABASE SCOPED CREDENTIAL AzureStorageCredential +WITH IDENTITY = 'IDENTITY'
+                """,
+                True,
+            ),
+            # azure is mentioned within the 2 lines below
+            (
+                """
+                lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==
+                some data 1
+                (https://portal.azure.com/). +CREATE DATABASE SCOPED CREDENTIAL AzureStorageCredential +WITH IDENTITY = 'IDENTITY'
+                """,
+                True,
+            ),
+            # azure is mentioned within the 3 lines below
+            (
+                """
+                lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==
+                some data 1
+                some data 2
+                (https://portal.azure.com/). +CREATE DATABASE SCOPED CREDENTIAL AzureStorageCredential +WITH IDENTITY = 'IDENTITY'
+                """,
+                False,
             ),
             (
                 """SshPublicKeys:
