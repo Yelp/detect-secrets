a3c4644949ea71a5d5d4758a4223f05673adcd33
Anatolii Stadnichuk <134394882+anatolii-paloaltonetworks@users.noreply.github.com>
2024-04-24 16:22:48 +0100

    Do not require leading quotes for high-entropy strings (#208)

--- tests/plugins/high_entropy_strings_test.py
+++ tests/plugins/high_entropy_strings_test.py
@@ -35,6 +35,14 @@ class TestHighEntropyString:
 
             # Non-quoted string
             ('{secret}', False),
+
+            # Non-quoted string from ini file
+            ('some_key = {secret}', True),
+            ('some_key={secret}', True),
+            #
+            # Non-quoted string from Yaml
+            ('some_key: {secret}', True),
+            ('some_key:{secret}', True),
         ),
     )
     def test_basic(plugin, non_secret, secret, format, should_be_caught):
