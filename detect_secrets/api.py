d737b680a964969d903d5df8dee32d651896a18d
issabayevmk <root@qa-vm-missabayev.amust.local>
2024-06-14 12:08:12 +0200

    Modified API tests to improve coverage

--- detect_secrets/api.py
+++ detect_secrets/api.py
@@ -1,17 +1,23 @@
-from git import Repo, InvalidGitRepositoryError
+import os
+from typing import Dict
+from typing import List
+
+from git import Repo
+
 from detect_secrets import SecretsCollection
-from detect_secrets.settings import default_settings, transient_settings
+from detect_secrets.settings import default_settings
+from detect_secrets.settings import transient_settings
 
 
-def get_settings(filters=None, plugins=None):
+def get_settings(filters: list = None, plugins: list = None) -> Dict[str, List]:
     """
     Return used plugins and filters to be used to scan with provided params
     """
     if filters and not isinstance(filters, list):
-        raise ValueError(f"Error: 'filters' must be List object")
+        raise ValueError(f'Error: "{filters}" must be List object')
 
     if plugins and not isinstance(plugins, list):
-        raise ValueError(f"Error: 'plugins' must be List object")
+        raise ValueError(f'Error: "{plugins}" must be List object')
 
     if filters:
         filters_used = filters
@@ -19,20 +25,22 @@ def get_settings(filters=None, plugins=None):
         filters_used = []
         with default_settings() as settings:
             for key in settings.filters:
-                filters_used.append({'path': key}) 
+                filters_used.append({'path': key})
 
     if plugins:
         plugins_used = plugins
     else:
         plugins_used = []
         with default_settings() as settings:
-             for key in settings.plugins:
+            for key in settings.plugins:
                 plugins_used.append({'name': key})
 
-    return {"plugins": plugins_used, "filters": filters_used}
+    return {'plugins': plugins_used, 'filters': filters_used}
 
 
-def scan_string(string: str, filters=None, plugins=None):
+def scan_string(
+    string: str, filters: list = None, plugins: list = None,
+) -> Dict[str, List]:
     """
     Scan a string for secrets using detect-secrets with custom filters and plugins
 
@@ -42,13 +50,13 @@ def scan_string(string: str, filters=None, plugins=None):
     :return: Detected secrets in str format
     """
     if not isinstance(string, str):
-        raise ValueError(f"Error: '{string}' must be 'string' formatted path to a file")
+        raise ValueError(f"Error: '{string}' must be 'string' object")
 
     if filters and not isinstance(filters, list):
-        raise ValueError(f"Error: 'filters' must be List object")
+        raise ValueError(f"Error: '{filters}' must be List object")
 
     if plugins and not isinstance(plugins, list):
-        raise ValueError(f"Error: 'plugins' must be List object")
+        raise ValueError(f"Error: '{plugins}' must be List object")
 
     # Initialize a SecretsCollection
     secrets = SecretsCollection()
@@ -61,23 +69,20 @@ def scan_string(string: str, filters=None, plugins=None):
             secrets.scan_string(string)
         return secrets.json()
     elif filters and not plugins:
-        filters_used = filters
-        plugins_used = get_settings(plugins=plugins).get("plugins")
+        plugins = get_settings(plugins=plugins).get('plugins')
     elif not filters and plugins:
-        plugins_used = plugins
-        filters_used = get_settings(filters=filters).get("filters")
-    else:
-        filters_used = filters
-        plugins_used = plugins
-        # Scan the string
-    with transient_settings(
-        {"plugins_used": plugins_used, "filters_used": filters_used}
-    ) as settings:
+        filters = get_settings(filters=filters).get('filters')
+
+    # Scan the string
+    settings = transient_settings({'plugins_used': plugins, 'filters_used': filters})
+    with settings:
         secrets.scan_string(string)
         return secrets.json()
 
 
-def scan_file(filepath, filters=None, plugins=None):
+def scan_file(
+    filepath: str, filters: list = None, plugins: list = None,
+) -> Dict[str, List]:
     """
     Scan a file for secrets using detect-secrets with custom filters and plugins
 
@@ -88,20 +93,20 @@ def scan_file(filepath, filters=None, plugins=None):
     """
     if not isinstance(filepath, str):
         raise ValueError(
-            f"Error: '{filepath}' must be 'string' formatted path to a file"
+            f"Error: '{filepath}' must be 'string' formatted path to a file",
         )
 
     if filters and not isinstance(filters, list):
-        raise ValueError(f"Error: 'filters' must be List object")
+        raise ValueError(f"Error: '{filters}' must be List object")
 
     if plugins and not isinstance(plugins, list):
-        raise ValueError(f"Error: 'plugins' must be List object")
+        raise ValueError(f"Error: '{plugins}' must be List object")
 
     try:
-        with open(filepath, "r") as f:
+        with open(filepath, 'r') as f:
             f.read()
-    except Exception as e:
-        return e
+    except Exception:
+        raise ValueError(f"Error: Cannot read '{filepath}'")
     # Initialize a SecretsCollection
     secrets = SecretsCollection()
 
@@ -113,26 +118,25 @@ def scan_file(filepath, filters=None, plugins=None):
             secrets.scan_file(filepath)
         return secrets.json()
     elif filters and not plugins:
-        filters_used = filters
-        plugins_used = get_settings(plugins=plugins).get("plugins")
+        plugins = get_settings(plugins=plugins).get('plugins')
     elif not filters and plugins:
-        plugins_used = plugins
-        filters_used = get_settings(filters=filters).get("filters")
-    else:
-        filters_used = filters
-        plugins_used = plugins
+        filters = get_settings(filters=filters).get('filters')
 
     # Scan a file
-    with transient_settings(
-        {"plugins_used": plugins_used, "filters_used": filters_used}
-    ) as settings:
+    settings = transient_settings(
+        {'plugins_used': plugins, 'filters_used': filters},
+    )
+    with settings:
         secrets.scan_file(filepath)
         return secrets.json()
 
 
 def scan_git_repository(
-    repo_path: str, plugins=None, filters=None, scan_all_files: bool = False
-):
+    repo_path: str,
+    plugins: list = None,
+    filters: list = None,
+    scan_all_files: bool = False,
+) -> List[Dict]:
     """
     Scan a local Git repository for secrets using the specified plugins and filters
 
@@ -140,23 +144,23 @@ def scan_git_repository(
     :param repo_path: Path to the local Git repository
     :param filters: Custom filters for detect-secrets
     :param plugins: Custom plugins for detect-secrets
-    :param scan_all_files (bool): If True, scan all files in the repository. If False, scan only Git-tracked files.
+    :param scan_all_files (bool): Scan all files or only Git-tracked files.
     :return: Detected secrets in List format
     """
     if not isinstance(scan_all_files, bool):
-        raise ValueError(f"Error: 'scan_all_files' must be 'bool' type")
+        raise ValueError(f"Error: '{scan_all_files}' must be 'bool' type")
     if not isinstance(repo_path, str):
-        raise ValueError(f"Error: 'repo_path' must be 'str' type path to repository")
+        raise ValueError(f"Error: '{repo_path}' must be 'str' type path to repository")
 
     try:
         repo = Repo(repo_path)
         if repo.bare:
-            raise InvalidGitRepositoryError
+            raise ValueError(f"Error: '{repo_path}' is not a valid Git repository")
 
         files_to_scan = []
         if scan_all_files:
             for root, _, files in os.walk(repo_path):
-                if ".git" in root:
+                if '.git' in root:
                     continue
                 for file in files:
                     files_to_scan.append(os.path.join(root, file))
@@ -165,7 +169,7 @@ def scan_git_repository(
                 os.path.join(repo_path, item.a_path) for item in repo.index.diff(None)
             ]
             files_to_scan.extend(
-                [os.path.join(repo_path, item) for item in repo.untracked_files]
+                [os.path.join(repo_path, item) for item in repo.untracked_files],
             )
 
         results = []
@@ -175,5 +179,5 @@ def scan_git_repository(
                 results.append(secrets)
         return results
 
-    except InvalidGitRepositoryError:
-        raise ValueError(f"Error: '{repo_path}' is not a valid Git repositoty")
+    except Exception:
+        raise ValueError(f"Error: '{repo_path}' is not a valid Git repository")
