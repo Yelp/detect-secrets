41f774616d3b0a736561fecb6dad830130b8ffd7
Pepe Fagoaga <jfagoagas@gmail.com>
2024-09-03 09:09:41 +0200

    fix(telegram_bot_token): `bot` should not be in the token
    `bot` is only used while verifying the token, as per https://core.telegram.org/bots/api#authorizing-your-bot

--- tests/plugins/telegram_token_test.py
+++ tests/plugins/telegram_token_test.py
@@ -8,7 +8,7 @@ class TestTelegramTokenDetector:
     @pytest.mark.parametrize(
         'payload, should_flag',
         [
-            ('bot110201543:AAHdqTcvCH1vGWJxfSe1ofSAs0K5PALDsaw', True),
+            ('110201543:AAHdqTcvCH1vGWJxfSe1ofSAs0K5PALDsaw', True),
             ('110201543:AAHdqTcvCH1vGWJxfSe1ofSAs0K5PALDsaw', True),
             ('7213808860:AAH1bjqpKKW3maRSPAxzIU-0v6xNuq2-NjM', True),
             ('foo:AAH1bjqpKKW3maRSPAxzIU-0v6xNuq2-NjM', False),
