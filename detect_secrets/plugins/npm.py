3e5b09836394b2833128c085518a9ff26af32532
matansha <matanshati@gmail.com>
2024-09-09 10:04:07 +0300

    Improve npm detection (#220)
    Co-authored-by: Matan Shati <>

--- detect_secrets/plugins/npm.py
+++ detect_secrets/plugins/npm.py
@@ -13,5 +13,8 @@ class NpmDetector(RegexBasedDetector):
     denylist = [
         # npmrc authToken
         # ref. https://stackoverflow.com/questions/53099434/using-auth-tokens-in-npmrc
-        re.compile(r'\/\/.+\/:_authToken=\s*((npm_.+)|([A-Fa-f0-9-]{36})).*'),
+        re.compile(
+            r'(?:npm_[A-Za-z0-9-]{36})|' +
+            r'(?:(?:_authToken|NPM[ _-]?TOKEN)[\s\S]{0,5}?(?:NpmToken\.)?([a-z0-9-]{36}).*)',
+        ),
     ]
