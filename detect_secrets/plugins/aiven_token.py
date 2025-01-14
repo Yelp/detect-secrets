be5d68c5a04b5609deac0ca001310e4dcb17f63e
kagahd <kay.agahd@idealo.de>
2025-01-14 09:34:10 +0100

    add a detector for Aiven token

---
+++ detect_secrets/plugins/aiven_token.py
@@ -0,0 +1,16 @@
+"""
+This plugin searches for Aiven tokens
+"""
+import re
+
+from detect_secrets.plugins.base import RegexBasedDetector
+
+
+class AivenTokenDetector(RegexBasedDetector):
+    """Scans for Aiven tokens."""
+    secret_type = 'Aiven Token'
+
+    denylist = [
+        # Aiven tokens follow the pattern: AVNS_<alphanumeric and underscores with a minimum length of 8>
+        re.compile(r'AVNS_[A-Za-z0-9_]{8,}'),
+    ]
---
+++ tests/plugins/aiven_token_test.py
@@ -0,0 +1,24 @@
+import pytest
+
+from detect_secrets.plugins.aiven_token import AivenTokenDetector
+
+
+class TestAivenTokenDetector:
+
+    @pytest.mark.parametrize(
+        'payload, should_flag',
+        [
+            ('AVNS_4Yt6Gdnjcs8ivIlYSFU', True),
+            ('AVNS_D0j9bUsCyQ3s67T', True),
+            ('AVNS_LaGqz39AC', True),
+            ('AVNS_RaFIf_JzHxFXlKs', True),
+            ('AVNS_UahLjsENr4QexJ1', True),
+            ('foo', False),
+            ('AVNS_', False),  # Incomplete token
+            ('AVNS12345678', False),  # Missing underscore
+        ],
+    )
+    def test_analyze(self, payload, should_flag):
+        logic = AivenTokenDetector()
+        output = logic.analyze_line(filename='mock_filename', line=payload)
+        assert len(output) == int(should_flag)
