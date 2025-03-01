f78fe692ad6fd1274195af6eddb4f70c532a5ccb
Miloslav Homer <miloslav.homer@gmail.com>
2025-03-01 17:05:57 +0100

    feat: fix #313 + other new slack tokens

--- detect_secrets/plugins/slack.py
+++ detect_secrets/plugins/slack.py
@@ -18,7 +18,7 @@ class SlackDetector(RegexBasedDetector):
 
     denylist = (
         # Slack Token
-        re.compile(r'xox(?:a|b|p|o|s|r)-(?:\d+-)+[a-z0-9]+', flags=re.IGNORECASE),
+        re.compile(r'(xwfp|xapp|xox(?:a|b|c|p|o|s|r))-(?:\d+-)+[a-z0-9]+', flags=re.IGNORECASE),
         # Slack Webhooks
         re.compile(
             r'https://hooks\.slack\.com/services/T[a-zA-Z0-9_]+/B[a-zA-Z0-9_]+/[a-zA-Z0-9_]+',
